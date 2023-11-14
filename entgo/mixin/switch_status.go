0a73bddde5e0062fecbee74725d5484fbb991080
devexps <thangn@vng.com.vn>
2023-11-14 17:04:44 +0700

    feat: add new mixin

---
+++ entgo/mixin/switch_status.go
@@ -0,0 +1,24 @@
+package mixin
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+	"entgo.io/ent/schema/mixin"
+)
+
+type SwitchStatus struct {
+	mixin.Schema
+}
+
+func (SwitchStatus) Fields() []ent.Field {
+	return []ent.Field{
+		field.Enum("status").
+			Comment("status").
+			Optional().
+			Default("ON").
+			Values(
+				"OFF",
+				"ON",
+			),
+	}
+}
